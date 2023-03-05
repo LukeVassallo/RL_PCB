d69bc5a6924a0232f1ec1f11e9ddacf6e0e58160
Luke Vassallo <lukevassallo95@gmail.com>
2023-03-05 11:10:01 +0100

    Updated test_setup.pu

--- test_setup.py
+++ test_setup.py
@@ -3,7 +3,56 @@ import graph.graph as graph
 import torch 
 from datetime import datetime
 
+from cpuinfo import get_cpu_info
+import psutil
+import numpy as np
+import optuna
+import pandas
+import matplotlib
+import seaborn
+
+import os, sys
+
 print("Program started at " + datetime.now().strftime("%Y-%m-%d %H:%M:%S:%f")+"\n")
+
+print(f'sysname            : {os.uname()[0]}')
+print(f'nodename           : {os.uname()[1]}')
+print(f'release            : {os.uname()[2]}')
+print(f'version            : {os.uname()[3]}')
+print(f'machine            : {os.uname()[4]}')
+print("")
+
+info = get_cpu_info()
+print(f'CPU arch           : {info["arch"]}')
+print(f'CPU bits           : {info["bits"]}')
+print(f'CPU brand          : {info["brand_raw"]}')
+print(f'CPU cores          : {info["count"]}')
+print(f'CPU base clock     : {info["hz_advertised_friendly"]}')
+print(f'CPU boost clock    : {info["hz_actual_friendly"]}')
+print(f'System Memory      : {np.round(psutil.virtual_memory().total / (1024**3),2)}GB')
+print("")
+
+#nvmlInit()
+#data.append(Paragraph(f'Nvidia driver version   : {str(nvmlSystemGetDriverVersion())}',style))
+#deviceCount = nvmlDeviceGetCount()
+#for i in range(deviceCount):
+    #handle = nvmlDeviceGetHandleByIndex(i)
+    #data.append(Paragraph(f'Device {i}                : {str(nvmlDeviceGetName(handle))}',style))
+    #data.append(Paragraph(f'Device {i}                : {np.round(nvmlDeviceGetMemoryInfo(handle).total / 1024**3,2)}GB',style))
+
+#nvmlShutdown()
+
+    #return data
+
+
+print(f'python             : {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}')
+print(f'torch              : {torch.__version__}')
+print(f'optuna             : {optuna.__version__}')
+print(f'numpy              : {np.__version__}')
+print(f'pandas             : {pandas.__version__}')
+print(f'matplotlib         : {matplotlib.__version__}')
+print(f'seaborn            : {seaborn.__version__}')
+print("")
 
 if torch.cuda.is_available():
     print(f"Using CUDA {torch.version.cuda}")
