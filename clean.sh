e963e4e98a883aeefe93b1eee797ac5f9ccb622c
Luke Vassallo <lukevassallo95@gmail.com>
2023-05-12 08:07:27 +0200

    Updated setup and clean scripts

--- clean.sh
+++ clean.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-if [ -d "${RL_PCB}/venv" ]; then
-	echo "Cleaning ${RL_PCB}/venv"
-	rm -fr ${RL_PCB}/venv
+DIR=${RL_PCB}/venv
+echo -n "Attempting to clean ${DIR} ... "
+if [ -d "${DIR}" ]; then
+	echo "Found, deleting."
+	rm -fr ${DIR} 
+else
+	echo "Not found, therefore nothing to clean."
 fi
 
-if [ -d "${RL_PCB}/bin" ]; then
-	echo "Cleaning ${RL_PCB}/bin"
-	rm -fr ${RL_PCB}/bin
+DIR=${RL_PCB}/bin
+echo -n "Attempting to clean ${DIR} ... "
+if [ -d "${DIR}" ]; then
+	echo "Found, deleting."
+	rm -fr ${DIR} 
+else
+	echo "Not found, therefore nothing to clean."
 fi
